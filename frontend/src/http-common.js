952a3bf8d33fcc43c75372ea88143b7974816480
Jordan <jordanenwright@gmail.com>
2022-12-17 13:29:55 +0000

    deployed to MongoDB
    built and deployed

--- frontend/src/http-common.js
+++ frontend/src/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://us-east-1.aws.webhooks.mongodb-realm.com/api/client/v2.0/app/restaurant-reviews-evuay/service/restaurants/incoming_webhook/",
+  baseURL: "https://eu-west-1.aws.data.mongodb-api.com/app/restaurant-reviews-kpqxg/endpoint/",
   headers: {
     "Content-type": "application/json"
   }
